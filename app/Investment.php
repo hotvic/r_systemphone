e555edb4895ea8821c54bb7fe795d7c41762e98b
Victor A <victoraur.santos@gmail.com>
2016-04-24 21:51:33 -0300

    add fillable fields

--- app/Investment.php
+++ app/Investment.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Investment extends Model
 {
+    protected $fillable = array('amount', 'description');
+    
     public function user()
     {
         return $this->belongsTo('\App\User');
