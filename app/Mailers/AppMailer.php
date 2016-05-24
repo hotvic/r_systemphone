37c43f99976527b52d62bc0b1298ea8c4b1c67f1
Victor A <victoraur.santos@gmail.com>
2016-05-24 15:21:45 -0300

    update sender info on emails

--- app/Mailers/AppMailer.php
+++ app/Mailers/AppMailer.php
@@ -6,12 +6,12 @@ use Mail;
 
 class AppMailer
 {
-    protected $from = 'contato@globalbetbrasil.com';
+    protected $from = 'contato@systemphonne.com';
 
     public function sendConfirmation($to)
     {
         Mail::send('emails.signup', ['user' => $to], function ($m) use ($to) {
-            $m->from($this->from, 'Global Bet Brasil');
+            $m->from($this->from, 'SystemPhone');
 
             $m->to($to->email, $to->name)->subject('Confirmação de E-Mail');
         });
