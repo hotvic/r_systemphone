18aee5635115d81e1aac1016e8fce1925aad7801
Victor A <victoraur.santos@gmail.com>
2016-05-27 17:43:29 -0300

    dashbaord balance display space

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -9,7 +9,7 @@
 @section('content')
     <div class="chute chute-center">
         <div class="row">
-            <div class="col-sm-4 col-xl-3">
+            <div class="col-sm-3 col-xl-4">
                 <div class="panel panel-tile info-block info-block-bg-success">
                     <div class="panel-body">
                         <div class="row">
@@ -30,7 +30,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-sm-4 col-xl-3">
+            <div class="col-sm-4 col-xl-4">
                 <div class="panel panel-tile info-block info-block-bg-info">
                     <div class="panel-body">
                         <div class="row">
@@ -51,14 +51,14 @@
                     </div>
                 </div>
             </div>
-            <div class="col-sm-4 col-xl-3">
+            <div class="col-sm-5 col-xl-4">
                 <div class="panel panel-tile info-block info-block-bg-warning">
                     <div class="panel-body">
                         <div class="row">
                             <div class="col-xs-5 ph10 text-center">
                                 <i class="fa fa-money"></i>
                             </div>
-                            <div class="col-xs-7 pl35 text-center">
+                            <div class="col-xs-7 pl10 text-center">
                                 <h2>{{ $stats->get('balance.total') }}</h2>
                                 <h6>Saldo Total</h6>
                             </div>
